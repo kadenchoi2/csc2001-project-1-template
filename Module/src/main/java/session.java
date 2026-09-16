139528f86676329e837c72cfcec90724ef221cf3
kadenchoi2 <kchoi42@calpoly.edu>
2026-09-16 10:40:25 -0700

    added hi
    hi

--- Module/src/main/java/session.java
+++ Module/src/main/java/session.java
@@ -1,4 +1,7 @@
 public record session(String id, String title, String mentor, String date, String location, int maxPar) {
+
+    public static void Main(String[] args){
+
     
-    public static void Main(String[] args){}
+    System.out.println("Hello");}
 }
